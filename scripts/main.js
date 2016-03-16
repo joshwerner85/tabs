05a69d82258a832f42bb94cd82b8c96bcb09e8c7
Josh <you@example.coexit>
2016-03-16 08:04:24 -0500

    almost complete

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+ function makeTabs(selector){
+    tabs_lists_anchors = document.querySelectorAll(selector + " li a");
+    divs = document.querySelector(selector).getElementsByTagName("div");
+    for (var i = 0; i < tab_lists_anchors.length; i++) {
+        if
+    }
+ }
