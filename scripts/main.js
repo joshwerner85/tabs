548777f3332b31443e8f9bcc7e6b64f5596ad808
Josh <you@example.coexit>
2016-04-08 03:12:32 -0500

    final version 3.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,28 @@
- function makeTabs(selector){
-    tabs_lists_anchors = document.querySelectorAll(selector + " li a");
-    divs = document.querySelector(selector).getElementsByTagName("div");
-    for (var i = 0; i < tab_lists_anchors.length; i++) {
-        if
-    }
- }
+var tabButton1 = document.querySelector('.tab1');
+var tabButton2 = document.querySelector('.tab2');
+var tabButton3 = document.querySelector('.tab3');
+var redPage = $('.red');
+var greenPage = $('.green');
+var bluePage = $('.blue');
+
+
+tabButton1.addEventListener('click', function (){
+	console.log('You clicked on the red tab');
+	redPage.show();
+	greenPage.hide();
+	bluePage.hide();
+});
+
+tabButton2.addEventListener('click', function (){
+	console.log('You clicked on the green tab');
+	redPage.hide();
+	greenPage.show();
+	bluePage.hide();
+});
+
+tabButton3.addEventListener('click', function (){
+	console.log('You clicked on the blue tab');
+	redPage.hide();
+	greenPage.hide();
+	bluePage.show();
+});
